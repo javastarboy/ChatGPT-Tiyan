ee670dbe396d39dac48aee0193a33878aae4f99c
JavaStar93 <43975677+javastarboy@users.noreply.github.com>
2023-07-11 06:01:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/components/sidebar.tsx
@@ -0,0 +1,346 @@
+import { useEffect, useRef, useState } from "react";
+
+import styles from "./home.module.scss";
+
+import { IconButton } from "./button";
+import SettingsIcon from "../icons/settings.svg";
+import GithubIcon from "../icons/github.svg";
+import ChatGptIcon from "../icons/chatgpt.svg";
+import AddIcon from "../icons/add.svg";
+import NoticeIcon from "../icons/notice.svg";
+import CloseIcon from "../icons/close.svg";
+import MaskIcon from "../icons/mask.svg";
+import PluginIcon from "../icons/plugin.svg";
+
+import Locale from "../locales";
+
+import { useAppConfig, useChatStore } from "../store";
+
+import {
+  MAX_SIDEBAR_WIDTH,
+  MIN_SIDEBAR_WIDTH,
+  NARROW_SIDEBAR_WIDTH,
+  Path,
+  REPO_URL,
+} from "../constant";
+
+import { Link, useNavigate } from "react-router-dom";
+import { useMobileScreen } from "../utils";
+import dynamic from "next/dynamic";
+import { showToast, Modal } from "./ui-lib";
+
+const ChatList = dynamic(async () => (await import("./chat-list")).ChatList, {
+  loading: () => null,
+});
+
+function useHotKey() {
+  const chatStore = useChatStore();
+
+  useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.metaKey || e.altKey || e.ctrlKey) {
+        const n = chatStore.sessions.length;
+        const limit = (x: number) => (x + n) % n;
+        const i = chatStore.currentSessionIndex;
+        if (e.key === "ArrowUp") {
+          chatStore.selectSession(limit(i - 1));
+        } else if (e.key === "ArrowDown") {
+          chatStore.selectSession(limit(i + 1));
+        }
+      }
+    };
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  });
+}
+
+function useDragSideBar() {
+  const limit = (x: number) => Math.min(MAX_SIDEBAR_WIDTH, x);
+
+  const config = useAppConfig();
+  const startX = useRef(0);
+  const startDragWidth = useRef(config.sidebarWidth ?? 300);
+  const lastUpdateTime = useRef(Date.now());
+
+  const handleMouseMove = useRef((e: MouseEvent) => {
+    if (Date.now() < lastUpdateTime.current + 50) {
+      return;
+    }
+    lastUpdateTime.current = Date.now();
+    const d = e.clientX - startX.current;
+    const nextWidth = limit(startDragWidth.current + d);
+    config.update((config) => (config.sidebarWidth = nextWidth));
+  });
+
+  const handleMouseUp = useRef(() => {
+    startDragWidth.current = config.sidebarWidth ?? 300;
+    window.removeEventListener("mousemove", handleMouseMove.current);
+    window.removeEventListener("mouseup", handleMouseUp.current);
+  });
+
+  const onDragMouseDown = (e: MouseEvent) => {
+    startX.current = e.clientX;
+
+    window.addEventListener("mousemove", handleMouseMove.current);
+    window.addEventListener("mouseup", handleMouseUp.current);
+  };
+  const isMobileScreen = useMobileScreen();
+  const shouldNarrow =
+    !isMobileScreen && config.sidebarWidth < MIN_SIDEBAR_WIDTH;
+
+  useEffect(() => {
+    const barWidth = shouldNarrow
+      ? NARROW_SIDEBAR_WIDTH
+      : limit(config.sidebarWidth ?? 300);
+    const sideBarWidth = isMobileScreen ? "100vw" : `${barWidth}px`;
+    document.documentElement.style.setProperty("--sidebar-width", sideBarWidth);
+  }, [config.sidebarWidth, isMobileScreen, shouldNarrow]);
+
+  return {
+    onDragMouseDown,
+    shouldNarrow,
+  };
+}
+
+export function SideBar(props: { className?: string }) {
+  const chatStore = useChatStore();
+
+  // drag side bar
+  const { onDragMouseDown, shouldNarrow } = useDragSideBar();
+  const navigate = useNavigate();
+  const config = useAppConfig();
+  // 公告弹窗用的组件
+  const [showDialog, setShowDialog] = useState(false); // 控制对话框的显示状态
+
+  useHotKey();
+
+  return (
+    <div
+      className={`${styles.sidebar} ${props.className} ${
+        shouldNarrow && styles["narrow-sidebar"]
+      }`}
+    >
+      <div className={styles["sidebar-header"]} data-tauri-drag-region>
+        <div className={styles["sidebar-title"]} data-tauri-drag-region>
+          ChatGPT4 Javastarboy
+        </div>
+        <div className={styles["sidebar-sub-title"]}>
+          <a
+            href="https://mp.weixin.qq.com/s/7rEZNtEPSdtwySki_pvPDw"
+            target="_blank"
+          >
+            感兴趣的小伙伴,加入社群,终身免费使用！
+          </a>{" "}
+          <br />
+          <a
+            href="https://www.jsbcp.top/%E5%85%AC%E4%BC%97%E5%8F%B7%E4%BA%8C%E7%BB%B4%E7%A0%81.jpg"
+            target="_blank"
+          >
+            点我关注公众号,每日惊喜不断,干货不断！
+          </a>
+        </div>
+        <div className={styles["sidebar-logo"] + " no-dark"}>
+          <ChatGptIcon />
+        </div>
+      </div>
+
+      <div className={styles["sidebar-header-bar"]}>
+        <IconButton
+          icon={<MaskIcon />}
+          text={shouldNarrow ? undefined : Locale.Mask.Name}
+          className={styles["sidebar-bar-button"]}
+          onClick={() => navigate(Path.NewChat, { state: { fromHome: true } })}
+          shadow
+        />
+        <IconButton
+          icon={<PluginIcon />}
+          text={shouldNarrow ? undefined : Locale.Plugin.Name}
+          className={styles["sidebar-bar-button"]}
+          onClick={() => showToast(Locale.WIP)}
+          shadow
+        />
+      </div>
+
+      <div
+        className={styles["sidebar-body"]}
+        onClick={(e) => {
+          if (e.target === e.currentTarget) {
+            navigate(Path.Home);
+          }
+        }}
+      >
+        <ChatList narrow={shouldNarrow} />
+      </div>
+
+      <div className={styles["sidebar-tail"]}>
+        <div className={styles["sidebar-actions"]}>
+          <div className={styles["sidebar-action"] + " " + styles.mobile}>
+            <IconButton
+              icon={<CloseIcon />}
+              onClick={() => {
+                if (confirm(Locale.Home.DeleteChat)) {
+                  chatStore.deleteSession(chatStore.currentSessionIndex);
+                }
+              }}
+            />
+          </div>
+          <div className={styles["sidebar-action"]}>
+            <Link to={Path.Settings}>
+              <IconButton icon={<SettingsIcon />} shadow />
+            </Link>
+          </div>
+          {/* github 小猫腻按钮 */}
+          {/* <div className={styles["sidebar-action"]}>
+            <a href={REPO_URL} target="_blank">
+              <IconButton icon={<GithubIcon />} shadow />
+            </a>
+          </div> */}
+        </div>
+        <div>
+          <IconButton
+            icon={<NoticeIcon />}
+            text={shouldNarrow ? undefined : Locale.Notice.Name}
+            className={`${styles["sidebar-bar-button"]} ${styles["centered-button"]}`}
+            onClick={() => {
+              setShowDialog(true); // 展示公告弹窗
+              console.log("showDialog===" + showDialog);
+            }}
+            shadow
+          />
+
+          {/* 公共弹窗 */}
+          {showDialog && (
+            <div className="modal-mask">
+              <Modal
+                title={"📣 公 告（本站已支持GPT-4，星球成员免费使用）"}
+                // title={
+                //   <span style={{ fontSize: "24px", textAlign: "center" }}>
+                //     📣 公 告
+                //   </span>
+                // }
+                onClose={() => setShowDialog(false)}
+                actions={[
+                  <IconButton
+                    key="close"
+                    bordered
+                    text={"关闭"}
+                    onClick={() => {
+                      setShowDialog(false),
+                        console.log("showDialog2===" + showDialog);
+                    }}
+                  />,
+                  <IconButton
+                    key="talk"
+                    bordered
+                    text={"交流"}
+                    onClick={() => {
+                      window.open(
+                        "https://www.jsbcp.top/%E5%BE%AE%E4%BF%A1%E4%BA%A4%E6%B5%81%E7%BE%A4.png",
+                      );
+                    }}
+                  />,
+                  <IconButton
+                    key="support"
+                    bordered
+                    text={"赞助"}
+                    onClick={() => {
+                      window.open(
+                        "https://www.jsbcp.top/%E5%BE%AE%E4%BF%A1%E6%94%B6%E6%AC%BE%E7%A0%81.jpeg",
+                      );
+                    }}
+                  />,
+                  <IconButton
+                    key="go"
+                    bordered
+                    text={"前往社群"}
+                    onClick={() => {
+                      window.open(
+                        "https://mp.weixin.qq.com/s/7rEZNtEPSdtwySki_pvPDw",
+                      );
+                    }}
+                  />,
+                ]}
+              >
+                <div className={styles["markdown-body"]}>
+                  ✅ 永久免费版▶{" "}
+                  <a href="https://www.jsbcp.top/" target="_blank">
+                    https://www.jsbcp.top/{" "}
+                  </a>
+                  <span style={{ color: "red" }}>
+                    &nbsp;【网址每月更新，建议加微信群，以便及时获取最新地址】
+                  </span>
+                  <br />
+                  <br />
+                  {/* <span>{Locale.Notice.Content}</span><br/><br/> */}✅
+                  免费送价值 298 元 AIGC 学习手册 ▶&nbsp;&nbsp;
+                  {/* 【涵盖ChatGPT、AI绘画、变现案例、行业报告、GPT部署...】&nbsp;<br/>&nbsp;&nbsp;&nbsp;&nbsp; */}
+                  <a
+                    href="https://ydyrb84oyc.feishu.cn/sheets/OfKvsq41MhRF5wt2kafcrR7lnVg"
+                    target="_blank"
+                  >
+                    【全局目录导航】
+                  </a>{" "}
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a
+                    href="https://ydyrb84oyc.feishu.cn/docx/UVLydQxKnowuqmx5mAycm7RdnJg"
+                    target="_blank"
+                  >
+                    【飞书目录合集】
+                  </a>{" "}
+                  <br />
+                  <br />✅ 欢迎来聊聊 (加微信拉入交流群)👉🏻{" "}
+                  <a
+                    href="https://www.jsbcp.top/%E5%BE%AE%E4%BF%A1%E4%BA%A4%E6%B5%81%E7%BE%A4.png"
+                    target="_blank"
+                  >
+                    AI2.0 实验室|微信交流群
+                  </a>{" "}
+                  <br />
+                  <br />✅ 社群简介请点击▶{" "}
+                  <a
+                    href="https://mp.weixin.qq.com/s/7rEZNtEPSdtwySki_pvPDw"
+                    target="_blank"
+                  >
+                    【人工智能变现学院-AI🔥2.0 实验室】
+                  </a>
+                  <br />
+                  <br />✅ 若需要 GPT-4 转发key▶{" "}
+                  <a
+                    href="https://www.jsbcp.top/%E5%BE%AE%E4%BF%A1%E4%BA%A4%E6%B5%81%E7%BE%A4.png"
+                    target="_blank"
+                  >
+                    也可以加我微信 javastarboy 💰1刀/3人民币50刀起，请求稳定！
+                  </a>
+                  <br />
+                  <br />
+                </div>
+              </Modal>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <IconButton
+            icon={<AddIcon />}
+            text={shouldNarrow ? undefined : Locale.Home.NewChat}
+            onClick={() => {
+              if (config.dontShowMaskSplashScreen) {
+                chatStore.newSession();
+                navigate(Path.Chat);
+              } else {
+                navigate(Path.NewChat);
+              }
+            }}
+            shadow
+          />
+        </div>
+      </div>
+
+      <div
+        className={styles["sidebar-drag"]}
+        onMouseDown={(e) => onDragMouseDown(e as any)}
+      ></div>
+    </div>
+  );
+}
---
+++ app/locales/cn.ts
@@ -0,0 +1,307 @@
+import { SubmitKey } from "../store/config";
+
+const cn = {
+  WIP: "该功能仍在开发中……",
+  Error: {
+    Unauthorized:
+      "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码，或者在[设置](/#/settings)页输入正确的访问密码，或者填入你自己的 OpenAI API Key。\n\n 1. 若无密码，可前往公众号👉🏻 [javastarboy](https://www.jsbcp.top/%E5%85%AC%E4%BC%97%E5%8F%B7%E4%BA%8C%E7%BB%B4%E7%A0%81.jpg) 回复**GPT4密码**获取密码。\n2. 欢迎来聊聊，每日分享干货👉🏻 [AI2.0 实验室|微信交流群](https://www.jsbcp.top/%E5%BE%AE%E4%BF%A1%E4%BA%A4%E6%B5%81%E7%BE%A4.png) \n3. 若需要 GPT-4 转发key，也可以加我微信 **javastarboy** 💰1刀/3人民币50刀起，请求稳定！\n4. 永久免费极速版ChatGPT网址请移步：https://www.jsbcp.top/ \n5. 免费送价值298学习手册\n - 全局目录导航：https://ydyrb84oyc.feishu.cn/sheets/OfKvsq41MhRF5wt2kafcrR7lnVg\n - 飞书目录合集：https://ydyrb84oyc.feishu.cn/docx/UVLydQxKnowuqmx5mAycm7RdnJg\n6. 关于【人工智能变现学院（AI2.0实验室）】社群简介请点击链接：https://mp.weixin.qq.com/s/7rEZNtEPSdtwySki_pvPDw \n\n【社群成员终身免费享有ChatGPT4使用密码】",
+  },
+  Auth: {
+    Title: "需要密码",
+    Tips: "管理员开启了密码验证，请在下方填入访问码",
+    Input: "在此处填写访问码",
+    Confirm: "确认",
+    Later: "稍后再说",
+  },
+  ChatItem: {
+    ChatItemCount: (count: number) => `${count} 条对话`,
+  },
+  Chat: {
+    SubTitle: (count: number) => `与 ChatGPT 的 ${count} 条对话`,
+    Actions: {
+      ChatList: "查看消息列表",
+      CompressedHistory: "查看压缩后的历史 Prompt",
+      Export: "导出聊天记录",
+      Copy: "复制",
+      Stop: "停止",
+      Retry: "重试",
+      Delete: "删除",
+    },
+    InputActions: {
+      Stop: "停止响应",
+      ToBottom: "滚到最新",
+      Theme: {
+        auto: "自动主题",
+        light: "亮色模式",
+        dark: "深色模式",
+      },
+      Prompt: "快捷指令",
+      Masks: "所有面具",
+      Clear: "清除聊天",
+      Settings: "对话设置",
+    },
+    Rename: "重命名对话",
+    Typing: "正在输入…",
+    Input: (submitKey: string) => {
+      var inputHints = `${submitKey} 发送`;
+      if (submitKey === String(SubmitKey.Enter)) {
+        inputHints += "，Shift + Enter 换行";
+      }
+      return inputHints + "，/ 触发补全";
+    },
+    Send: "发送",
+    Config: {
+      Reset: "清除记忆",
+      SaveAs: "存为面具",
+    },
+  },
+  Export: {
+    Title: "分享聊天记录",
+    Copy: "全部复制",
+    Download: "下载文件",
+    Share: "分享到 ShareGPT",
+    MessageFromYou: "来自你的消息",
+    MessageFromChatGPT: "来自 ChatGPT 的消息",
+    Format: {
+      Title: "导出格式",
+      SubTitle: "可以导出 Markdown 文本或者 PNG 图片",
+    },
+    IncludeContext: {
+      Title: "包含面具上下文",
+      SubTitle: "是否在消息中展示面具上下文",
+    },
+    Steps: {
+      Select: "选取",
+      Preview: "预览",
+    },
+  },
+  Select: {
+    Search: "搜索消息",
+    All: "选取全部",
+    Latest: "最近几条",
+    Clear: "清除选中",
+  },
+  Memory: {
+    Title: "历史摘要",
+    EmptyContent: "对话内容过短，无需总结",
+    Send: "自动压缩聊天记录并作为上下文发送",
+    Copy: "复制摘要",
+    Reset: "[unused]",
+    ResetConfirm: "确认清空历史摘要？",
+  },
+  Home: {
+    NewChat: "新的聊天",
+    DeleteChat: "确认删除选中的对话？",
+    DeleteToast: "已删除会话",
+    Revert: "撤销",
+  },
+  Notice: {
+    Name: "通知公告",
+    Content:
+      "📣重要通知📣 \n👉🏻 加我微信交流更多 javastarboy \n👉🏻 点击确定了解【人工智能变现学院-AI🔥2.0 实验室】",
+  },
+  Settings: {
+    Title: "设置",
+    SubTitle: "设置选项",
+    Actions: {
+      ClearAll: "清除所有数据",
+      ResetAll: "重置所有选项",
+      Close: "关闭",
+      ConfirmResetAll: "确认重置所有配置？",
+      ConfirmClearAll: "确认清除所有数据？",
+    },
+    Lang: {
+      Name: "Language", // ATTENTION: if you wanna add a new translation, please do not translate this value, leave it as `Language`
+      All: "所有语言",
+    },
+    Avatar: "头像",
+    FontSize: {
+      Title: "字体大小",
+      SubTitle: "聊天内容的字体大小",
+    },
+
+    Update: {
+      Version: (x: string) => `当前版本：${x}`,
+      IsLatest: "已是最新版本",
+      CheckUpdate: "检查更新",
+      IsChecking: "正在检查更新...",
+      FoundUpdate: (x: string) => `发现新版本：${x}`,
+      GoToUpdate: "前往更新",
+    },
+    SendKey: "发送键",
+    Theme: "主题",
+    TightBorder: "无边框模式",
+    SendPreviewBubble: {
+      Title: "预览气泡",
+      SubTitle: "在预览气泡中预览 Markdown 内容",
+    },
+    Mask: {
+      Title: "面具启动页",
+      SubTitle: "新建聊天时，展示面具启动页",
+    },
+    Prompt: {
+      Disable: {
+        Title: "禁用提示词自动补全",
+        SubTitle: "在输入框开头输入 / 即可触发自动补全",
+      },
+      List: "自定义提示词列表",
+      ListCount: (builtin: number, custom: number) =>
+        `内置 ${builtin} 条，用户定义 ${custom} 条`,
+      Edit: "编辑",
+      Modal: {
+        Title: "提示词列表",
+        Add: "新建",
+        Search: "搜索提示词",
+      },
+      EditModal: {
+        Title: "编辑提示词",
+      },
+    },
+    HistoryCount: {
+      Title: "附带历史消息数",
+      SubTitle: "每次请求携带的历史消息数",
+    },
+    CompressThreshold: {
+      Title: "历史消息长度压缩阈值",
+      SubTitle: "当未压缩的历史消息超过该值时，将进行压缩",
+    },
+    Token: {
+      Title: "API Key",
+      SubTitle: "使用自己的 Key 可绕过密码访问限制",
+      Placeholder: "OpenAI API Key",
+    },
+
+    Usage: {
+      Title: "余额查询",
+      SubTitle(used: any, total: any) {
+        return `本月已使用 $${used}，订阅总额 $${total}`;
+      },
+      IsChecking: "正在检查…",
+      Check: "重新检查",
+      NoAccess: "输入 API Key 或访问密码查看余额",
+    },
+    AccessCode: {
+      Title: "访问密码",
+      SubTitle: "管理员已开启加密访问",
+      Placeholder: "请输入访问密码",
+    },
+    Endpoint: {
+      Title: "接口地址",
+      SubTitle: "除默认地址外，必须包含 http(s)://",
+    },
+    Model: "模型 (model)",
+    Temperature: {
+      Title: "随机性 (temperature)",
+      SubTitle: "值越大，回复越随机",
+    },
+    MaxTokens: {
+      Title: "单次回复限制 (max_tokens)",
+      SubTitle: "单次交互所用的最大 Token 数",
+    },
+    PresencePenalty: {
+      Title: "话题新鲜度 (presence_penalty)",
+      SubTitle: "值越大，越有可能扩展到新话题",
+    },
+    FrequencyPenalty: {
+      Title: "频率惩罚度 (frequency_penalty)",
+      SubTitle: "值越大，越有可能降低重复字词",
+    },
+  },
+  Store: {
+    DefaultTopic: "新的聊天",
+    BotHello:
+      "前往公众号👉🏻 [javastarboy](https://www.jsbcp.top/%E5%85%AC%E4%BC%97%E5%8F%B7%E4%BA%8C%E7%BB%B4%E7%A0%81.jpg) 回复**GPT4密码**获取密码，然后点击左下角[设置](/#/settings)按钮输入密码后**即可使用**。\n\n若需要 GPT-4 转发key，也可以加我微信 **javastarboy** 💰1刀/3人民币50刀起，请求稳定！\n\n学习目录导航索引（群公告视频号视频必看）：\n- 全局目录导航：https://ydyrb84oyc.feishu.cn/sheets/OfKvsq41MhRF5wt2kafcrR7lnVg\n- 飞书目录合集：https://ydyrb84oyc.feishu.cn/docx/UVLydQxKnowuqmx5mAycm7RdnJg \n\n欢迎来聊聊👉🏻 [AI2.0 实验室|微信交流群](https://www.jsbcp.top/%E5%BE%AE%E4%BF%A1%E4%BA%A4%E6%B5%81%E7%BE%A4.png) ",
+    Error: "出错了，稍后重试吧",
+    Prompt: {
+      History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
+      Topic:
+        "使用四到五个字直接返回这句话的简要主题，不要解释、不要标点、不要语气词、不要多余文本，如果没有主题，请直接返回“闲聊”",
+      Summarize:
+        "简要总结一下对话内容，用作后续的上下文提示 prompt，控制在 200 字以内",
+    },
+  },
+  Copy: {
+    Success: "已写入剪切板",
+    Failed: "复制失败，请赋予剪切板权限",
+  },
+  Context: {
+    Toast: (x: any) => `包含 ${x} 条预设提示词`,
+    Edit: "当前对话设置",
+    Add: "新增预设对话",
+    Clear: "上下文已清除",
+    Revert: "恢复上下文",
+  },
+  Plugin: {
+    Name: "插件",
+  },
+  Mask: {
+    Name: "面具",
+    Page: {
+      Title: "预设角色面具",
+      SubTitle: (count: number) => `${count} 个预设角色定义`,
+      Search: "搜索角色面具",
+      Create: "新建",
+    },
+    Item: {
+      Info: (count: number) => `包含 ${count} 条预设对话`,
+      Chat: "对话",
+      View: "查看",
+      Edit: "编辑",
+      Delete: "删除",
+      DeleteConfirm: "确认删除？",
+    },
+    EditModal: {
+      Title: (readonly: boolean) =>
+        `编辑预设面具 ${readonly ? "（只读）" : ""}`,
+      Download: "下载预设",
+      Clone: "克隆预设",
+    },
+    Config: {
+      Avatar: "角色头像",
+      Name: "角色名称",
+      Sync: {
+        Title: "使用全局设置",
+        SubTitle: "当前对话是否使用全局模型设置",
+        Confirm: "当前对话的自定义设置将会被自动覆盖，确认启用全局设置？",
+      },
+      HideContext: {
+        Title: "隐藏预设对话",
+        SubTitle: "隐藏后预设对话不会出现在聊天界面",
+      },
+    },
+  },
+  NewChat: {
+    Return: "返回",
+    Skip: "直接开始",
+    NotShow: "不再展示",
+    ConfirmNoShow: "确认禁用？禁用后可以随时在设置中重新启用。",
+    Title: "挑选一个面具",
+    SubTitle: "现在开始，与面具背后的灵魂思维碰撞",
+    More: "查看全部",
+  },
+
+  UI: {
+    Confirm: "确认",
+    Cancel: "取消",
+    Close: "关闭",
+    Create: "新建",
+    Edit: "编辑",
+  },
+  Exporter: {
+    Model: "模型",
+    Messages: "消息",
+    Topic: "主题",
+    Time: "时间",
+  },
+};
+
+type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
+
+export type LocaleType = typeof cn;
+export type PartialLocaleType = DeepPartial<typeof cn>;
+
+export default cn;
