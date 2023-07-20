cf0e4040b2cd434fa9ed4d5ce4bc5a86be8bb98a
JavaStarBoy <594086597@qq.com>
2023-07-20 16:45:24 +0800

    禁用 4.0模型

--- app/store/config.ts
+++ app/store/config.ts
@@ -55,13 +55,13 @@ const ENABLE_GPT4 = true;
 
 export const ALL_MODELS = [
   {
-    name: "gpt-4",
-    available: ENABLE_GPT4,
+    name: "gpt4请加微信单独开通",
+    available: false,
   },
-  {
-    name: "gpt-4-32k",
-    available: ENABLE_GPT4,
-  },
+  // {
+  //   name: "gpt-4-32k",
+  //   available: ENABLE_GPT4,
+  // },
   {
     name: "gpt-3.5-turbo",
     available: true,
