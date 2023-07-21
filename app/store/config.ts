0f79c833674ead780f70bd706b2c55c41c403441
JavaStarBoy <594086597@qq.com>
2023-07-21 09:22:12 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -58,6 +58,14 @@ export const ALL_MODELS = [
     name: "gpt-4",
     available: ENABLE_GPT4,
   },
+  {
+    name: "gpt-4-0314",
+    available: ENABLE_GPT4,
+  },
+  {
+    name: "gpt-4-0613",
+    available: ENABLE_GPT4,
+  },
   // {
   //   name: "gpt-4-32k",
   //   available: ENABLE_GPT4,
@@ -66,10 +74,10 @@ export const ALL_MODELS = [
     name: "gpt-3.5-turbo",
     available: true,
   },
-  // {
-  //   name: "gpt-3.5-turbo-16k",
-  //   available: true,
-  // },
+  {
+    name: "gpt-3.5-turbo-16k",
+    available: true,
+  },
   {
     name: "qwen-v1", // 通义千问
     available: false,
